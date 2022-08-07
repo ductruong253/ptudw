b902f5e39f822c9e94ab243cf0042052c29888d5
ductruong253 <ddtruong94@gmail.com>
2022-08-08 00:17:30 +0700

    Update lab nang cao EX04

--- lab/20880108_JS_NangCao/20880108_Ex01.js
+++ lab/20880108_JS_NangCao/20880108_Ex01.js
@@ -1,6 +1,7 @@
 'use strict'
 const helper = require('./20880108_Ex03');
 const helperSlow = require('./20880108_Ex05');
+let slowRandom = require('./20880108_Ex04');
 
 const dsSinhVien = [
     {
@@ -161,16 +162,21 @@ const dsSinhVien = [
 ]
 
 console.log('>>>>>     Danh sach sinh vien duoc khoi tao: ');
-console.log (JSON.stringify(dsSinhVien));
+console.log(JSON.stringify(dsSinhVien));
 console.log('>>>>>     Danh sach sinh vien sau khi chay generate: ');
 helper.generate(dsSinhVien);
-console.log (JSON.stringify(dsSinhVien));
-console.log('>>>>>     Danh sach sinh vien sau khi chay average: ');
-helper.average(dsSinhVien);
-console.log (JSON.stringify(dsSinhVien));
-console.log('>>>>>     Danh sach sinh vien sau khi chay normalize: ');
-helper.normalize(dsSinhVien);
-console.log (JSON.stringify(dsSinhVien));
-console.log('>>>>>     Danh sach sinh vien sau khi chay slow-generate: ');
-helperSlow.generate(dsSinhVien);
-console.log (JSON.stringify(dsSinhVien));
+// console.log(JSON.stringify(dsSinhVien));
+// console.log('>>>>>     Danh sach sinh vien sau khi chay average: ');
+// helper.average(dsSinhVien);
+// console.log(JSON.stringify(dsSinhVien));
+// console.log('>>>>>     Danh sach sinh vien sau khi chay normalize: ');
+// helper.normalize(dsSinhVien);
+// console.log(JSON.stringify(dsSinhVien));
+// console.log('>>>>>     Danh sach sinh vien sau khi chay slow-generate: ');
+// helperSlow.generate(dsSinhVien);
+// console.log(JSON.stringify(dsSinhVien));
+
+(async function () {
+    let x = await slowRandom(0, 10);
+    console.log(x);
+})()
